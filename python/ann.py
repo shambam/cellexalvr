d418f96261bd393129e1832c32ce4d942f9e5480
d <d>
2020-08-17 18:39:41 +0200

    m

--- python/ann.py
+++ python/ann.py
@@ -1,14 +1,18 @@
 import anndata
 import sys
 import numpy as np
+import json
 
 np.set_printoptions(threshold=sys.maxsize,linewidth=np.inf)
 file_name = sys.argv[1]
-f = anndata.h5py.File(file_name,'r')
+if file_name.endswith(".loom"):
+    f = anndata.h5py.File(file_name,'r')
+else:
+    f = anndata.read_h5ad(file_name,'r')
 while True:
     interp = input()
     try:
-        exec("print("+interp+")")
+        exec("print(json.dumps("+interp+"))")
     	#exec("print(np.array2string("+interp+", separator=','))")
     except Exception as e:
     	print(e)
